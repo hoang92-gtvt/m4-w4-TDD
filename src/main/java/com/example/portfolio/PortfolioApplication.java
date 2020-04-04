85f4d4b9cdb76fcfc9c0a138f0bf6174b230cc7d
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-04-04 14:58:52 +0700

    add pgsql

--- src/main/java/com/example/portfolio/PortfolioApplication.java
+++ src/main/java/com/example/portfolio/PortfolioApplication.java
@@ -2,10 +2,12 @@ package com.example.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.example.portfolio.repository")
+@EntityScan("com.example.portfolio.model")
 public class PortfolioApplication {
 
     public static void main(String[] args) {
