205aa514dc23d9628a8fb7e89675ece7c897033d
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-17 17:05:29 +0700

    create new class done

--- src/main/java/com/example/portfolio/controller/ClassesController.java
+++ src/main/java/com/example/portfolio/controller/ClassesController.java
@@ -5,9 +5,7 @@ import com.example.portfolio.service.classes.IClassesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -18,5 +16,10 @@ public class ClassesController {
     @GetMapping("/classes")
     public ResponseEntity<Iterable<Classes>> getAllClasses() {
         return new ResponseEntity<>(classesService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/classes")
+    public ResponseEntity<Classes> createNewClass(@RequestBody Classes classes) {
+        return new ResponseEntity<>(classesService.save(classes), HttpStatus.OK);
     }
 }
