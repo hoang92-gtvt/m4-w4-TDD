c1a99c0240a5f17bde9201182dd92bb5e0537260
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-17 16:21:59 +0700

    add Student repository and service

---
+++ src/main/java/com/example/portfolio/service/student/IStudentService.java
@@ -0,0 +1,7 @@
+package com.example.portfolio.service.student;
+
+import com.example.portfolio.model.Student;
+import com.example.portfolio.service.GeneralService;
+
+public interface IStudentService extends GeneralService<Student> {
+}
