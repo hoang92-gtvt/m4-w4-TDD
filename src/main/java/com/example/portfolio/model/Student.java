78b5f607a5a098225d59ded078aab79a5783d4c8
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-17 16:15:29 +0700

    add Spring Profile

---
+++ src/main/java/com/example/portfolio/model/Student.java
@@ -0,0 +1,20 @@
+package com.example.portfolio.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String studentId;
+
+    @Column(nullable = false)
+    private String name;
+}
