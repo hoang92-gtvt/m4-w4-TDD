a1fca9d37decdae5e9d051792c4c7f6f5f7070ea
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-17 16:28:50 +0700

    create new student

--- src/main/java/com/example/portfolio/controller/StudentController.java
+++ src/main/java/com/example/portfolio/controller/StudentController.java
@@ -5,9 +5,7 @@ import com.example.portfolio.service.student.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -18,5 +16,10 @@ public class StudentController {
     @GetMapping("/students")
     public ResponseEntity<Iterable<Student>> getAllStudents() {
         return new ResponseEntity<>(studentService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/students")
+    public ResponseEntity<Student> createNewStudent(@RequestBody Student student) {
+        return new ResponseEntity<>(studentService.save(student), HttpStatus.OK);
     }
 }
