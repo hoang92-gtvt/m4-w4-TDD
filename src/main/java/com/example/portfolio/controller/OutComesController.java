2b75177108c2559f8352ea4fdf485b5e765610ea
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-04-25 11:14:18 +0700

    crud outcomes

--- src/main/java/com/example/portfolio/controller/OutComesController.java
+++ src/main/java/com/example/portfolio/controller/OutComesController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @CrossOrigin("*")
 public class OutComesController {
@@ -20,4 +22,39 @@ public class OutComesController {
         Iterable<OutComes> outComes = outComesService.findAll();
         return new ResponseEntity<>(outComes, HttpStatus.OK);
     }
+
+    @PostMapping("/outcomes")
+    public ResponseEntity<OutComes> createOutComes(@RequestBody OutComes outComes){
+        return new ResponseEntity<>(outComesService.save(outComes), HttpStatus.OK);
+    }
+
+    @GetMapping("/outcomes/{id}")
+    public ResponseEntity<OutComes> getOutComes(@PathVariable Long id){
+        Optional<OutComes> optionalOutComes = outComesService.findById(id);
+        if (!optionalOutComes.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(optionalOutComes.get(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/outcomes/{id}")
+    public ResponseEntity<OutComes> deleteOutComes(@PathVariable Long id){
+        Optional<OutComes> optionalOutComes = outComesService.findById(id);
+        if (!optionalOutComes.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        outComesService.remove(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/outcomes/{id}")
+    public ResponseEntity<OutComes> editOutComes(@PathVariable Long id, @RequestBody OutComes outComes){
+        Optional<OutComes> optionalOutComes = outComesService.findById(id);
+        if (!optionalOutComes.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        outComes.setId(id);
+        return new ResponseEntity<>(outComesService.save(outComes), HttpStatus.OK);
+    }
+
 }
